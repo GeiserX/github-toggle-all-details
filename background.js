b2b72fd8f857d06921f5b004f268d4add259c9ff
FERNS154 <sergio.fernandez.-nd@disney.com>
2025-09-05 13:56:16 +0200

    chore: delete comments

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.action.onClicked.addListener((tab) => {
   if (!tab || !tab.id) return;
   chrome.tabs.sendMessage(tab.id, { type: "TOGGLE_GH_DETAILS" }).catch(() => {
-    // No content script on this page or permissions mismatch; ignore.
   });
 });
